4c320650bfdced53207d7cc82fc5a39b5b450c80
Ben <benjamin.etheredge@gmail.com>
2021-09-17 23:40:12 +0000

    testing deployments

--- src/battler/deploy/showdown.py
+++ src/battler/deploy/showdown.py
@@ -1,17 +1,21 @@
 import asyncio
 import os
 
+from poke_env.player.player import Player
 from poke_env.player_configuration import PlayerConfiguration
 from poke_env.server_configuration import ShowdownServerConfiguration
 
+MAX_CONCURRENT_BATTLE = 100
+N_BATTLES = 100
 
-async def main(player_constructor):
+async def main(player_constructor: Player):
     # We create a random player
     player = player_constructor(
         player_configuration=PlayerConfiguration(os.environ["USERNAME"], os.environ["PASSWORD"]),
         server_configuration=ShowdownServerConfiguration,
     )
-    await player.accept_challenges(None, 5)
+
+    await player.accept_challenges(None, N_BATTLES)
     for battle in player.battles.values():
         try:
             print(battle.rating, battle.opponent_rating)
