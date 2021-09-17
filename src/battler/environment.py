6ff82b9a2bb95fa3a921d1254d0ae790b7e4a1c1
Ben <benjamin.etheredge@gmail.com>
2021-09-17 23:40:40 +0000

    scratch code

---
+++ src/battler/environment.py
@@ -0,0 +1,89 @@
+from tf_agents.environments import py_environment
+from tf_agents.environments import arr
+import numpy as np
+
+
+from poke_env.player.env_player import Gen8EnvSinglePlayer
+
+class SimpleRLPlayer(Gen8EnvSinglePlayer):
+    def embed_battle(self, battle):
+        # -1 indicates that the move does not have a base power
+        # or is not available
+        moves_base_power = -np.ones(4)
+        moves_dmg_multiplier = np.ones(4)
+        for i, move in enumerate(battle.available_moves):
+            moves_base_power[i] = move.base_power / 100 # Simple rescaling to facilitate learning
+            if move.type:
+                moves_dmg_multiplier[i] = move.type.damage_multiplier(
+                    battle.opponent_active_pokemon.type_1,
+                    battle.opponent_active_pokemon.type_2,
+                )
+
+        # We count how many pokemons have not fainted in each team
+        remaining_mon_team = len([mon for mon in battle.team.values() if mon.fainted]) / 6
+        remaining_mon_opponent = (
+            len([mon for mon in battle.opponent_team.values() if mon.fainted]) / 6
+        )
+
+        # Final vector with 10 components
+        return np.concatenate(
+            [moves_base_power, moves_dmg_multiplier, [remaining_mon_team, remaining_mon_opponent]]
+        )
+
+    def compute_reward(self, battle) -> float:
+        return self.reward_computing_helper(
+            battle,
+            fainted_value=2,
+            hp_value=1,
+            victory_value=30,
+        )
+
+class PokeEnv(py_environment.PyEnvironment):
+
+  def __init__(self):
+    self._action_spec = array_spec.BoundedArraySpec(
+        shape=(), dtype=np.int32, minimum=0, maximum=1, name='action')
+    self._observation_spec = array_spec.BoundedArraySpec(
+        shape=(1,), dtype=np.int32, minimum=0, name='observation')
+    self._state = 0
+    self._episode_ended = False
+
+  def action_spec(self):
+    return self._action_spec
+
+  def observation_spec(self):
+    return self._observation_spec
+
+  def _reset(self):
+    self._state = 0
+    self._episode_ended = False
+    return ts.restart(np.array([self._state], dtype=np.int32))
+
+  def _step(self, action):
+
+    if self._episode_ended:
+      # The last action ended the episode. Ignore the current action and start
+      # a new episode.
+      return self.reset()
+
+    # Make sure episodes don't go on forever.
+    if action == 1:
+      self._episode_ended = True
+    elif action == 0:
+      new_card = np.random.randint(1, 11)
+      self._state += new_card
+    else:
+      raise ValueError('`action` should be 0 or 1.')
+
+    if self._episode_ended or self._state >= 21:
+      reward = self._state - 21 if self._state <= 21 else -21
+      return ts.termination(np.array([self._state], dtype=np.int32), reward)
+    else:
+      return ts.transition(
+          np.array([self._state], dtype=np.int32), reward=0.0, discount=1.0)
+class PokemonEnvironment(py_environment.PyEnvironment):
+    def __init__(self, env):
+        self._env = env
+        self._state = None
+
+        self._action_spec = 
