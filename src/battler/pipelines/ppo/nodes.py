7f224f654d53b48926c5e2f24fe542153af893e8
Ben <benjamin.etheredge@gmail.com>
2021-10-02 04:39:15 +0000

    Cleanup

--- src/battler/pipelines/ppo/nodes.py
+++ src/battler/pipelines/ppo/nodes.py
@@ -10,48 +10,18 @@ import string
 from poke_env.player_configuration import PlayerConfiguration
 import pytorch_lightning as pl
 
-
-def train_wrapper(
-    env_name, other, kwargs, env
-):
-    trainer = Trainer(
-        gpus=0,
-        accelerator='dp',
-        max_epochs=1000,
-        #log_every_n_steps=1,
-        flush_logs_every_n_steps=4_000,
-        logger=pl.loggers.MLFlowLogger(
-            experiment_name='ppo_lightning',
-        )
-        #logger=pl.loggers.TensorBoardLogger(
-            #'.',
-            #log_graph=True,
-            #default_hp_metric=False,
-            #save_dir='tb_logs',
-            #name='ppo_logs',
-        #)
-    )
-    model = PPOLightning(env_name, env_kwargs=kwargs, raw_env=env)
-    trainer.fit(model)
-    return model
-
-from poke_env.player.env_player import DummyPlayer
 # TODO use attention and pad moves
 # TODO trainer kwargs
 def train(
     lightning_kwargs: Dict,
-    epochs: int,
+    trainer_kwargs: Dict,
 ) -> Dict:
 
     # TODO change eval to challange existing players by id
     pl.seed_everything(4)
 
     trainer = Trainer(
-        gpus=1,
-        accelerator='dp',
-        max_epochs=epochs,
-        #log_every_n_steps=1,
-        flush_logs_every_n_steps=4_000,
+        **trainer_kwargs
         logger=pl.loggers.MLFlowLogger(
             experiment_name='ppo_lightning',
         )
